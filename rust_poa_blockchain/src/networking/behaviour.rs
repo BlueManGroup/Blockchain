1adb77e79191482f6afc6e0c8bbdc73097636d98
Christian <creich21@student.aau.dk>
2024-05-08 10:06:42 +0200

    DM implemented in Behaviour

--- rust_poa_blockchain/src/networking/behaviour.rs
+++ rust_poa_blockchain/src/networking/behaviour.rs
@@ -2,16 +2,19 @@ use libp2p::{
     floodsub::Floodsub,
     swarm::NetworkBehaviour,
     PeerId, identify, 
-    mdns, request_response
+    mdns, request_response,
+    StreamProtocol
 };
 use libp2p_identity::PublicKey;
 use crate::networking::reqres;
+use crate::networking::behaviour::request_response::ProtocolSupport;
 
 #[derive(NetworkBehaviour)]
 pub struct Behaviour {
     pub floodsub: Floodsub,
     pub identify: identify::Behaviour,
     pub mdns: mdns::async_io::Behaviour,
+    pub reqres: request_response::json::Behaviour::<reqres::GreetRequest, reqres::GreetResponse>,
 } 
 
 impl Behaviour {
@@ -20,44 +23,9 @@ impl Behaviour {
         let identify = identify::Behaviour::new(identify::Config::new("1.0".into(), local_public_key));
         let mdns_config = mdns::Config::default();
         let mdns = mdns::async_io::Behaviour::new(mdns_config, local_peer_id)?;
-        Ok(Self {identify, floodsub,mdns})
+        let reqres = request_response::json::Behaviour::<reqres::GreetRequest, reqres::GreetResponse>::new(
+            [(StreamProtocol::new("/JsonDirectMessage"), ProtocolSupport::Full)],
+            request_response::Config::default());
+        Ok(Self {identify, floodsub,mdns, reqres})
     }
 }
-// fn main() {
-//     // Generate a new identity keypair
-//     let identity_keys = identity::Keypair::generate_ed25519();
-//     let local_peer_id = PeerId::from(identity_keys.public());
-
-//     // Create the behaviour
-//     let behaviour = Behaviour::new(local_peer_id.clone()).expect("Failed to create behaviour");
-
-//     // Create the swarm
-//     let swarm = SwarmBuilder::new(behaviour, local_peer_id.clone(), identity_keys)
-//         .executor(Box::new(|fut| {
-//             tokio::spawn(fut);
-//         }))
-//         .build();
-
-//     // Start listening on a random TCP port
-//     let listen_addr: Multiaddr = "/ip4/0.0.0.0/tcp/0".parse().expect("Failed to parse listen address");
-//     Swarm::listen_on(&swarm, listen_addr).expect("Failed to listen on address");
-
-//     // Start the main event loop
-//     tokio::run(futures::future::poll_fn(move || {
-//         loop {
-//             match swarm.poll().expect("Failed to poll swarm") {
-//                 Async::Ready(Some(event)) => {
-//                     match event {
-//                         // Handle floodsub events
-//                         BehaviourEvent::Floodsub(FloodsubEvent::Message { message, .. }) => {
-//                             println!("Received message: {:?}", String::from_utf8_lossy(&message.data));
-//                         }
-//                         _ => {}
-//                     }
-//                 }
-//                 Async::Ready(None) | Async::NotReady => break,
-//             }
-//         }
-//         Ok(Async::NotReady)
-//     }));
-// }
--- rust_poa_blockchain/src/networking/reqres.rs
+++ rust_poa_blockchain/src/networking/reqres.rs
@@ -1,42 +1,14 @@
 use libp2p;
 use async_std;
 
-struct DMProtocol();
 
-impl libp2p::request_response::ProtocolName for DMProtocol {
-    fn protocol_name(&self) -> &[u8] {
-        "/DMProtocol/1.0.0".as_bytes()
-    }
+
+#[derive(Debug, serde::Serialize, serde::Deserialize)]
+pub struct GreetRequest {
+    name: String,
 }
 
-
-
-
-struct DMCodec();
-
-#[async_trait]
-impl RequestResponseCodec for DMCodec {
-    type Protocol = DMProtocol;
-    type Request = String;
-    type Response = String;
-
-    async fn read_request(&mut self, io: &mut impl async_std::io::ReadExt) -> std::io::Result<Self::Request> {
-        let mut buf = Vec::new();
-        io.read_to_end(&mut buf).await?;
-        Ok(String::from_utf8(buf).unwrap())
-    }
-
-    async fn read_response(&mut self, io: &mut impl async_std::io::ReadExt) -> std::io::Result<Self::Response> {
-        let mut buf = Vec::new();
-        io.read_to_end(&mut buf).await?;
-        Ok(String::from_utf8(buf).unwrap())
-    }
-
-    async fn write_request(&mut self, io: &mut impl async_std::io::WriteExt, request: Self::Request) -> std::io::Result<()> {
-        io.write_all(request.as_bytes()).await
-    }
-
-    async fn write_response(&mut self, io: &mut impl async_std::io::WriteExt, response: Self::Response) -> std::io::Result<()> {
-        io.write_all(response.as_bytes()).await
-    }
+#[derive(Debug, serde::Serialize, serde::Deserialize)]
+pub struct GreetResponse {
+    message: String,
 }
